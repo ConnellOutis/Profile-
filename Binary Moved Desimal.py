30a250c50caf8da9cb55d1e39c6b594a4f6e5a46
Connell Outis <90070062+ConnellOutis@users.noreply.github.com>
2021-11-29 06:53:46 -0800

    Add files via upload
    Program . Binary moved Desimal By Connell Outis . 
    By Python

---
+++ Binary Moved Desimal.py
@@ -0,0 +1,28 @@
+Ali = 0
+
+Outis = 0
+
+Connell = 1
+
+CNO = 0
+
+Ali = int(input("Enter Binary Number :"))
+
+while Ali :
+
+    Outis = int(Ali%10)
+
+    Ali = int(Ali/10)
+
+    Outis *= Connell
+
+    CNO += Outis
+
+    Connell = Connell*2
+
+print(CNO)
+
+
+
+
+
